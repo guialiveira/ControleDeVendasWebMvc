339a46388d79d3c0ba306bf655064d9c29e477ea
Guilherme Alves <guii05@hotmail.com>
2020-08-15 16:22:24 -0300

    gerando CRUD automatico

---
+++ ControleDeVendasWebMvc/Data/ControleDeVendasWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ControleDeVendasWebMvc.Models;
+
+namespace ControleDeVendasWebMvc.Data
+{
+    public class ControleDeVendasWebMvcContext : DbContext
+    {
+        public ControleDeVendasWebMvcContext (DbContextOptions<ControleDeVendasWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ControleDeVendasWebMvc.Models.Department> Department { get; set; }
+    }
+}
