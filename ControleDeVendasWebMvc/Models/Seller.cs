359bcf90f8a478b992e8e5b42bc0e6b50a39a2fe
Guilherme Alves <guii05@hotmail.com>
2020-08-16 19:09:58 -0300

    Dando um departamento para o vendedor antes de criar

--- ControleDeVendasWebMvc/Models/Seller.cs
+++ ControleDeVendasWebMvc/Models/Seller.cs
@@ -14,6 +14,7 @@ namespace ControleDeVendasWebMvc.Models
         public double BaseSalary { get; set; }
 
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
 
         public ICollection<SellesRecord> Sales { get; set; } = new List<SellesRecord>();
 
--- ControleDeVendasWebMvc/Services/SellerService.cs
+++ ControleDeVendasWebMvc/Services/SellerService.cs
@@ -24,6 +24,7 @@ namespace ControleDeVendasWebMvc.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First(); //solução pra dar um departamento pro vendedor
             _context.Add(obj);
             _context.SaveChanges();
 
