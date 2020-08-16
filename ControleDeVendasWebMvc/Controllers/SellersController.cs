fcc3b725d196431898e177474ac67fa2efe36b4e
Guilherme Alves <guii05@hotmail.com>
2020-08-16 18:03:15 -0300

    Create vendedor implementado

--- ControleDeVendasWebMvc/Controllers/SellersController.cs
+++ ControleDeVendasWebMvc/Controllers/SellersController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ControleDeVendasWebMvc.Models;
 using ControleDeVendasWebMvc.Services;
 using Microsoft.AspNetCore.Mvc;
 
+//Controlador de Vendedor
 namespace ControleDeVendasWebMvc.Controllers
 {
     public class SellersController : Controller
@@ -21,7 +23,16 @@ namespace ControleDeVendasWebMvc.Controllers
             var list = _sellerService.FindAll();
             return View(list);
         }
-
-        
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken] // previne ataque 
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
